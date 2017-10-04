745b4743bcc505f79b2eaab73627bae38ec44408
Iván Babic <ibabic.uy@gmail.com>
2017-10-04 18:28:52 -0300

    Finished two layer q

--- assignment1/cs231n/classifiers/neural_net.py
+++ assignment1/cs231n/classifiers/neural_net.py
@@ -116,13 +116,13 @@ class TwoLayerNet(object):
         dscores[range(N), y] -= 1
         dscores /= N
 
-        grads['b2'] = np.sum(dscores, axis=0, keepdims=True)
+        grads['b2'] = np.sum(dscores, axis=0)
         grads['W2'] = np.dot(a1.T, dscores) + reg * 2 * W2
 
         da1 = np.dot(dscores, W2.T)
         dh1 = da1
         dh1[h1 <= 0] = 0  # ReLu
-        grads['b1'] = np.sum(dh1, axis=0, keepdims=True)
+        grads['b1'] = np.sum(dh1, axis=0)
         grads['W1'] = np.dot(X.T, dh1) + reg * 2 * W1
         #############################################################################
         #                              END OF YOUR CODE                             #
@@ -160,46 +160,49 @@ class TwoLayerNet(object):
         val_acc_history = []
 
         for it in xrange(num_iters):
-            X_batch = None
-            y_batch = None
-
             #########################################################################
             # TODO: Create a random minibatch of training data and labels, storing  #
             # them in X_batch and y_batch respectively.                             #
             #########################################################################
-        pass
-        #########################################################################
-        #                             END OF YOUR CODE                          #
-        #########################################################################
+            choices = np.random.choice(num_train, batch_size, replace=False)
+            X_batch = X[choices, :]
+            y_batch = y[choices]
+            #########################################################################
+            #                             END OF YOUR CODE                          #
+            #########################################################################
 
-        # Compute loss and gradients using the current minibatch
-        loss, grads = self.loss(X_batch, y=y_batch, reg=reg)
-        loss_history.append(loss)
+            # Compute loss and gradients using the current minibatch
+            loss, grads = self.loss(X_batch, y=y_batch, reg=reg)
+            loss_history.append(loss)
 
-        #########################################################################
-        # TODO: Use the gradients in the grads dictionary to update the         #
-        # parameters of the network (stored in the dictionary self.params)      #
-        # using stochastic gradient descent. You'll need to use the gradients   #
-        # stored in the grads dictionary defined above.                         #
-        #########################################################################
-        pass
-        #########################################################################
-        #                             END OF YOUR CODE                          #
-        #########################################################################
-
-        if verbose and it % 100 == 0:
-            print('iteration %d / %d: loss %f' % (it, num_iters, loss))
-
-        # Every epoch, check train and val accuracy and decay learning rate.
-        if it % iterations_per_epoch == 0:
-            # Check accuracy
-            train_acc = (self.predict(X_batch) == y_batch).mean()
-            val_acc = (self.predict(X_val) == y_val).mean()
-            train_acc_history.append(train_acc)
-            val_acc_history.append(val_acc)
-
-            # Decay learning rate
+            #########################################################################
+            # TODO: Use the gradients in the grads dictionary to update the         #
+            # parameters of the network (stored in the dictionary self.params)      #
+            # using stochastic gradient descent. You'll need to use the gradients   #
+            # stored in the grads dictionary defined above.                         #
+            #########################################################################
+            self.params['W1'] -= learning_rate * grads['W1']
+            self.params['W2'] -= learning_rate * grads['W2']
+            self.params['b1'] -= learning_rate * grads['b1']
+            self.params['b2'] -= learning_rate * grads['b2']
             learning_rate *= learning_rate_decay
+            #########################################################################
+            #                             END OF YOUR CODE                          #
+            #########################################################################
+
+            if verbose and it % 100 == 0:
+                print('iteration %d / %d: loss %f' % (it, num_iters, loss))
+
+            # Every epoch, check train and val accuracy and decay learning rate.
+            if it % iterations_per_epoch == 0:
+                # Check accuracy
+                train_acc = (self.predict(X_batch) == y_batch).mean()
+                val_acc = (self.predict(X_val) == y_val).mean()
+                train_acc_history.append(train_acc)
+                val_acc_history.append(val_acc)
+
+                # Decay learning rate
+                learning_rate *= learning_rate_decay
 
         return {
             'loss_history': loss_history,
@@ -223,11 +226,11 @@ class TwoLayerNet(object):
           to have class c, where 0 <= c < C.
         """
         y_pred = None
-
         ###########################################################################
         # TODO: Implement this function; it should be VERY simple!                #
         ###########################################################################
-        pass
+        scores = self.loss(X)
+        y_pred = np.argmax(scores, axis=1)
         ###########################################################################
         #                              END OF YOUR CODE                           #
         ###########################################################################
